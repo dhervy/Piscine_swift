c407664fcbf0c13b495bdc27017bf239f0a97c4b
Duane HERVY <dhervy@e2r8p11.42.fr>
2019-01-15 22:20:07 +0100

    base_project to code on my mac

---
+++ base_project/base_project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  base_project
+//
+//  Created by Duane HERVY on 1/15/19.
+//  Copyright © 2019 Duane HERVY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
